6e6a677adace77e969ad9e6cbf95bb5708871a60
oldhuman <44600803+oldhuman@users.noreply.github.com>
2018-10-30 20:19:29 +0200

    Update reboot-all.sh

--- reboot-all.sh
+++ reboot-all.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
 for project in $(gcloud projects list  --format="value(project_id)")
 do
 	for instance in  $(gcloud compute instances list --format="value(name)" --filter="status:running" --quiet)
 	do
-	echo "Stopping instance : $project $instance"
+	echo "${GREEN}Stopping instance :${NOCOLOR} $project $instance"
 	  zone=$(gcloud compute instances list --filter="name=$instance" --format="value(zone)" --quiet)
 	  status=$(gcloud compute instances describe $instance --zone=$zone --format="value(status)" --quiet)
 	  gcloud compute instances stop $instance --zone=$zone --async --quiet
@@ -12,7 +16,7 @@ do
 	
 	for instance in  $(gcloud compute instances list --format="value(name)" --filter="status:terminated" --quiet)
 	do
-	echo "Starting instance : $project $instance"
+	echo "${GREEN}Starting instance :${NOCOLOR} $project $instance"
 	  zone=$(gcloud compute instances list --filter="name=$instance" --format="value(zone)" --quiet)
 	  status=$(gcloud compute instances describe $instance --zone=$zone --format="value(status)" --quiet)
 	  gcloud compute instances start $instance --zone=$zone --async --quiet
