72a7d1080a2b76e9ac748ceca2a8aa8ea9c364bb
oldhuman <44600803+oldhuman@users.noreply.github.com>
2018-10-30 14:42:12 +0200

    Create delete.sh

---
+++ delete.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+delete_instances (){
+for instance in  $(gcloud compute instances list --filter="status=running" --format="value(name)" --quiet)
+do
+  zone=$(gcloud compute instances list --filter="name=$instance" --format="value(zone)" --quiet)
+  status=$(gcloud compute instances describe $instance --zone=$zone --format="value(status)" --quiet)
+  created_on=$(gcloud compute instances describe $instance --zone=$zone --format="value(creationTimestamp.date('%Y-%m-%d'))" --quiet)
+  echo "####Deleting instance " $project "instance-"$i $zone
+  gcloud compute instances delete $instance --zone=$zone --quiet
+done
+}
+
+#LOGIC
+for project in $(gcloud projects list  --format="value(project_id)")
+	do
+		gcloud config set project $project
+		echo "Project: " $project
+		delete_instances
+	done
+exit
