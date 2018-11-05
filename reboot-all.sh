e216a1f392119a019eea3c4af57e4ea45502c503
oldhuman <oldhuman55@gmail.com>
2018-11-05 13:20:43 +0200

    add email-roles too reboot

--- reboot-all.sh
+++ reboot-all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 email="oldhuman55@gmail.com"
-for project in $(gcloud projects list  --format="value(project_id)")
+for project in $(gcloud projects list --format="value(project_id)")
 do
 	for instance in  $(gcloud compute instances list --format="value(name)" --filter="status:running" --quiet)
 		do
 		echo "Stopping instance: $project $instance"
 		  zone=$(gcloud compute instances list --filter="name=$instance" --format="value(zone)" --quiet)
 		  status=$(gcloud compute instances describe $instance --zone=$zone --format="value(status)" --quiet)
-		  gcloud compute instances stop $instance --zone=$zone --async --quiet
+		  gcloud compute instances stop $instance --zone=$zone --quiet
 		done
 done
 
-for project in $(gcloud projects list  --format="value(project_id)")
+for project in $(gcloud projects list --format="value(project_id)")
 do
 	gcloud projects add-iam-policy-binding $project --member user:$email --role roles/editor
 	for instance in  $(gcloud compute instances list --format="value(name)" --filter="status:terminated" --quiet)
