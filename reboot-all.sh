df25519f866795f63b1c2e91a02eb864549a639b
oldhuman <oldhuman55@gmail.com>
2018-11-05 14:00:36 +0200

    try fast reboots

--- reboot-all.sh
+++ reboot-all.sh
@@ -4,13 +4,13 @@ for project in $(gcloud projects list --format="value(project_id)")
 do
 	for instance in  $(gcloud compute instances list --format="value(name)" --filter="status:running" --quiet)
 		do
-		echo "##################"
-		echo "#####Restarting instance: $project $instance"
-		  zone=$(gcloud compute instances list --filter="name=$instance" --format="value(zone)" --quiet)
-		  status=$(gcloud compute instances describe $instance --zone=$zone --format="value(status)" --quiet)
-		  #gcloud compute instances stop $instance --zone=$zone --quiet
-		  gcloud compute instances reset $instance --zone=$zone --quiet
-		  echo "$instance $zone $status"
+			echo "##################"
+			echo "#####Restarting instance: $project $instance"
+			zone=$(gcloud compute instances list --filter="name=$instance" --format="value(zone)" --quiet)
+			status=$(gcloud compute instances describe $instance --zone=$zone --format="value(status)" --quiet)
+			gcloud compute instances reset $instance --zone=$zone --quiet
 		done
-		echo "$(gcloud compute instances list --format="value(name)")"
+
+	echo "$(gcloud compute instances list)"
+	echo ""
 done
